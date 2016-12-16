048b52c8ecfaf56c4ae1694b745fd46885bd8d34
zhangxuchao <2212362272@qq.com>
2016-12-16 14:07:02 +0800

    Update android.ts

--- android.ts
+++ android.ts
@@ -1,3 +1,7 @@
+cd到开发目录， 创建打包平台
+  ionic platform add android
+  ionic platform add ios 
+
 1. 下载安装jdk 1.8    http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
 2. 下载安装 android sdk     https://developer.android.com/studio/install.html?pkg=tools
 3. 下载更新不同版本的  sdk   config 配置更新
