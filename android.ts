b91701f5169455fbc5dc23e1eb0ecf43a48d4f1b
zhangxuchao <2212362272@qq.com>
2016-12-16 13:19:11 +0800

    Create android.ts

---
+++ android.ts
@@ -0,0 +1,6 @@
+1. 下载安装jdk 1.8    http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+2. 下载安装 android sdk     https://developer.android.com/studio/install.html?pkg=tools
+3. 下载更新不同版本的  sdk   config 配置更新
+
+
+4. ionic build android
